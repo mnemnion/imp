375caa9cb2dc73aed023452e8c2c2e6b0fad363e
sam atman <atmanistan@gmail.com>
2014-01-08 22:51:37 -0800

    makin box's

--- Forge/src/forge.fs
+++ Forge/src/forge.fs
@@ -2,14 +2,34 @@ include ansi.fs
 include toolbelt.fs
 include stack-util.fs
 
-anew wipeout
-
 \ inner loop
 
-variable eval-pad here 128 allot 
+variable eval-pad 128 allot 
 
 defer innerloop
 
-:noname 
-	eval-pad 128 accept eval-pad swap evaluate innerloop ;
-	is innerloop 
+\ we need some way to catch all exceptions here. 
+:noname  \ no more ." ok" cr
+	eval-pad 128 accept 
+	eval-pad swap evaluate 
+	innerloop ; is innerloop 
+
+: makebox 
+	create 
+		, , , , 
+	does>
+		dup 
+		2 cells + 
+		2@ 
+		rot 
+		2@ ;
+( 
+\ this does something cool:
+page 
+cols 2/ 
+dup 0 at-xy \ jump halfway on screen
+2 - rows 2/ 2 - swap .|box \ draw box 1/4 of screen
+cols 2/ 2 + 1 at-xy        \ jump back into box
+innerloop                  \ start i/oing.
+)
+anew wipeout

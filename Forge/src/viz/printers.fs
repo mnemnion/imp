593c1c24cb4e8e464bd02905a7417aa879902ff7
sam atman <atmanistan@gmail.com>
2014-02-07 21:10:57 -0800

    printing works, n-printables God Word gone

--- Forge/src/viz/printers.fs
+++ Forge/src/viz/printers.fs
@@ -1,43 +1,3 @@
-
-: n-printables \ ( [c-str] n -- count )
-	\ "takes a string buffer. returns the offset needed to print
-	\ "n characters, or a full line. ansi escaped."
-
-	 dup dup >r -rot r> -rot \ stash copies of n
-	 0 do 
-		dup 
-		c@ \ cr .s cr
-		dup #nl = if
-			i 0 = if
-				drop 1 +
-			else 
-				drop drop \ ( n n+ -- )
-				swap - i + 0 swap 0
-				leave \ 0 swap 0 protects against final drop/nip. craaaazy
-			then
-		else
-		   #esc 
-			<> if  
-			    1 +
-			else                 \ ( n adr #esc -- ) esc-code handler
-			    1 +      
-				begin
-					swap 1 + swap
-					dup c@ 
-						dup [char] m 
-						= if
-						    drop 1 + 
-						    true 
-						else
-							drop 1 + false
-						then
-				until 	
-			then
-		then
-	loop
-	drop nip \ drop advance adr and nip count, or 0 and 0 for newline.
-	;
-
 : esc-printables? 
 \ 	cr .m ." esc "
 	drop 1 swap
@@ -153,20 +113,6 @@
 	.!
 	;
 
-: skip-cr \ ( [c-str] -> [c-str] | [c-str+] )
-	\ "skip a leading newline"
-	dup 0 <> if 
-		swap dup c@ \ ( count c-adr char -- )
-		10 = if 
-	 	\ 	cr .bo ." cr skipped" .! 
-			1 + swap 1 - \ ( c-adr+ count+ -- )
-		\ 	cr .s ." "
-		else
-			swap
-		then
-	else then 
-	;
-
 : print-advance \ ( buf off n -> buf+ off- "string" )
 	over 0 <> if 
 		>r 2dup r@ printables 
@@ -182,29 +128,8 @@
 	else 
 		drop
 	then
-	.!
+\ 	.!
 	;
-
-: print-advance~ \ ( [c-str] n -> c-adr+ count- "string" )
-	\ 	"prints n characters, advances the address correspondingly "
-	\   
-		>r skip-cr 2dup drop r> 
-		n-printables
-	\ 	cr .m .s
-	2dup > if \ safe to print as counted
-		swap >r 2dup type r> over >r 
-	\ 	.! cr .cy .s .!
-		 swap -  
-	\ 	.! cr .y .s  .!
-		 swap r> 
-	\ 	.! cr .g .s .!
-		+ swap 
-	\ 	 cr .di .w .s .!
-	else      \ print the whole thing
-		drop dup >r 2dup type r@ - swap r> + swap
- 	then
-	; 
-
 
 
 : .title \ ( buf off frame -> nil -- "frame" )
